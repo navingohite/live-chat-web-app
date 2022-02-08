5a99ae7d2bb69c22af1a2b025e4ed54ad76b9656
Navin Gohite <navingohite07@gmail.com>
2022-02-08 23:10:39 +0530

    updated

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+session_start ();
+function loginForm() {
+    echo '
+	<div class="form-group">
+		<div id="loginform">
+			<form action="index.php" method="post">
+			<h1> <img src="images/logo/logo.png" alt="Live Chat"> </h1><hr/>
+				<label for="name">Join as a Guest!</label>
+				<input type="text" name="name" id="name" class="form-control" placeholder="Enter your name"/>
+                <br>
+				<input type="submit" class="btn btn-default" name="enter" id="enter" value="Login" />
+			</form>
+		</div>
+	</div>
+   ';
+}
+ 
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $cb = fopen ( "log.html", 'a' );
+        fwrite ( $cb, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat room.</i><br></div>" );
+        fclose ( $cb );
+    } else {
+        echo '<span class="error">Field cannot be empty</span>';
+    }
+}
+ 
+if (isset ( $_GET ['logout'] )) {
+    $cb = fopen ( "log.html", 'a' );
+    fwrite ( $cb, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat room.</i><br></div>" );
+    fclose ( $cb );
+    session_destroy ();
+    header ( "Location: index.php" );
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Live Chat!</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body style="background-color:bisque" >
+<?php
+	if (! isset ( $_SESSION ['name'] )) {
+	loginForm ();
+	} else {
+?>
+<div id="wrapper">
+	<div id="menu">
+	<h1><img src="images/logo/logo.png" alt="Live Chat"></h1><hr/>
+		<p class="welcome"><b>Welcome - <a><?php echo $_SESSION['name']; ?></a></b></p>
+		<p class="logout"><a id="exit" href="#" class="btn btn-default">Leave</a></p>
+	<div style="clear: both"></div>
+	</div>
+	<div id="chatbox">
+	<?php
+		if (file_exists ( "log.html" ) && filesize ( "log.html" ) > 0) {
+		$handle = fopen ( "log.html", "r" );
+		$contents = fread ( $handle, filesize ( "log.html" ) );
+		fclose ( $handle );
+
+		echo $contents;
+		}
+	?>
+	</div>
+<form name="message" action="">
+	<input name="usermsg" class="form-control" type="text" id="usermsg" placeholder="Create A Message" />
+    <br>
+	<input name="submitmsg" class="btn btn-sm" type="submit" id="submitmsg" value="Send" />
+</form>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+});
+$(document).ready(function(){
+    $("#exit").click(function(){
+        var exit = confirm("Are you sure that you want to leave ?");
+        if(exit==true){window.location = 'index.php?logout=true';}     
+    });
+});
+$("#submitmsg").click(function(){
+        var clientmsg = $("#usermsg").val();
+        $.post("post.php", {text: clientmsg});             
+        $("#usermsg").attr("value", "");
+        loadLog;
+    return false;
+});
+function loadLog(){    
+    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+    $.ajax({
+        url: "log.html",
+        cache: false,
+        success: function(html){       
+            $("#chatbox").html(html);       
+            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+            if(newscrollHeight > oldscrollHeight){
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal');
+            }              
+        },
+    });
+}
+setInterval (loadLog, 2500);
+</script>
+<?php
+}
+?>
+</body>
+</html>
